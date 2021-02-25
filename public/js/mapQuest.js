db48de00449fcfc5d54978fc8ddc2962d67a26c1
Irene Tang <idtang@ucsd.edu>
2021-02-25 10:51:13 -0800

    maps

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'KEY';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [37.7749, -122.4194],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12,
+	  zoomControl: false
+	});
+
+	// add a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
